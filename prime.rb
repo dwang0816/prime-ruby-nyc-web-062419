87eddab46a53584b5b5b00d4b214419bdde1f4e6
dennis wang <dwang0816@gmail.com>
2019-06-09 03:33:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,10 @@
 # Add  code here 
-def prime?(integer)
-  
+def prime?(num)
+    if num <= 1
+        false
+    elsif num == 2
+        true
+    else
+        (2..num/2).none? { |i| num % i == 0}
+    end
+end 
